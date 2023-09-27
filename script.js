bcfe99d0ea50b6ce30e35aec696a2f3a7336d80f
Ch00mba <146185209+Ch00mba@users.noreply.github.com>
2023-09-27 11:26:58 +0000

    5 dice roll randomly

--- script.js
+++ script.js
@@ -34,3 +34,44 @@ function createDice(number) {
 			[80, 80]
 		]
 	};
+
+  const dice = document.createElement("div");
+
+	dice.classList.add("dice");
+
+	for (const dotPosition of dotPositionMatrix[number]) {
+		const dot = document.createElement("div");
+
+		dot.classList.add("dice-dot");
+		dot.style.setProperty("--top", dotPosition[0] + "%");
+		dot.style.setProperty("--left", dotPosition[1] + "%");
+		dice.appendChild(dot);
+	}
+
+	return dice;
+}
+
+function randomizeDice(diceContainer, numberOfDice) {
+	diceContainer.innerHTML = "";
+
+	for (let i = 0; i < numberOfDice; i++) {
+		const random = Math.floor((Math.random() * 6) + 1);
+		const dice = createDice(random);
+
+		diceContainer.appendChild(dice);
+	}
+}
+
+const NUMBER_OF_DICE = 5;
+const diceContainer = document.querySelector(".dice-container");
+const btnRollDice = document.querySelector(".btn-roll-dice");
+
+randomizeDice(diceContainer, NUMBER_OF_DICE);
+
+btnRollDice.addEventListener("click", () => {
+	const interval = setInterval(() => {
+		randomizeDice(diceContainer, NUMBER_OF_DICE);
+	}, 50);
+
+	setTimeout(() => clearInterval(interval), 1000);
+});
